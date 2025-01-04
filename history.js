aadc0a9089fbd8298871ead8bed9730c38ef4362
goljavi <javiergold@live.com>
2025-01-04 14:04:27 -0300

    save results to file

--- history.js
+++ history.js
@@ -60,12 +60,32 @@ history.forEach((song) => {
 
 })
 
-console.log("Minutos escuchados: ", parseInt(sum/(60000)))
-console.log("Canciones escuchadas: ", songs.length)
-console.log("Canciones + escuchadas: ", songs.sort((a, b) => b.count - a.count).slice(0, 10).map((s) => `${s.trackName} (${s.artistName}) (${s.count} veces)`))
-console.log("Artistas escuchados: ", artistas.length)
-console.log("Artistas + escuchados por cantidad de canciones: ", artistas.sort((a, b) => b.uniqueSongs.length - a.uniqueSongs.length).slice(0, 10).map((s) => `${s.artistName} (${s.uniqueSongs.length} canciones)`))
-console.log("Artistas + escuchados por cantidad de tiempo: ", artistas.sort((a, b) => b.totalTime - a.totalTime).slice(0, 10).map((s) => `${s.artistName} (${parseInt(s.totalTime/60000)} minutos)`))
-console.log("Días que más música escuché: ", days.sort((a, b) => b.totalTime - a.totalTime).slice(0, 10).map((s) => `${s.endTime.split(" ")[0]} (${parseInt(s.totalTime/60000)} minutos) (${parseInt(s.totalTime/(3600000))} horas)`))
-console.log("Por día de la semana: ", conteoPorDiaSemana)
-console.log("Por mes: ", conteoPorMes)
+const fs = require('fs');
+
+const output = {
+    "Minutos escuchados": parseInt(sum / 60000),
+    "Canciones escuchadas": songs.length,
+    "Canciones más escuchadas": songs
+        .sort((a, b) => b.count - a.count)
+        .slice(0, 10)
+        .map((s) => `${s.trackName} (${s.artistName}) (${s.count} veces)`),
+    "Artistas escuchados": artistas.length,
+    "Artistas más escuchados por cantidad de canciones": artistas
+        .sort((a, b) => b.uniqueSongs.length - a.uniqueSongs.length)
+        .slice(0, 10)
+        .map((s) => `${s.artistName} (${s.uniqueSongs.length} canciones)`),
+    "Artistas más escuchados por cantidad de tiempo": artistas
+        .sort((a, b) => b.totalTime - a.totalTime)
+        .slice(0, 10)
+        .map((s) => `${s.artistName} (${parseInt(s.totalTime / 60000)} minutos)`),
+    "Días que más música escuché": days
+        .sort((a, b) => b.totalTime - a.totalTime)
+        .slice(0, 10)
+        .map((s) => `${s.endTime.split(" ")[0]} (${parseInt(s.totalTime / 60000)} minutos) (${parseInt(s.totalTime / 3600000)} horas)`),
+    "Por día de la semana": conteoPorDiaSemana,
+    "Por mes": conteoPorMes
+};
+
+fs.writeFileSync('spotify_summary.json', JSON.stringify(output, null, 2));
+
+console.log("Summary saved to 'spotify_summary.json'");
